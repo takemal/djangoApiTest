4946502ad826e6297c9824b8c027f0c1f24ba56c
takemal <take0piece.0715@icloud.com>
2022-11-07 23:49:58 +0900

    initial heroku commit

--- rest_api/settings.py
+++ rest_api/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nextjs-django-test.herokuapp.com']
 
 
 # Application definition
